84b7c27d36fc80b591bd8ee820e4cafae7f2055f
harmik-7span <harmik@7span.com>
2023-06-06 16:06:29 +0530

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectorRef, Component, OnInit, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-signal';
+  count = signal(0);
+  totalPrice = computed(() => this.count()*10);
+  color = computed(() => this.count()>5?'green':'blue');
+  e = effect(() => console.log('dh',this.totalPrice()));
+  x = signal(5)
+  y = signal(6)
+  z = computed(()=>this.x()+this.y());
+  a = 5;
+  b=6;
+  c= this.a+this.b;
+  
+  
+  constructor(private change:ChangeDetectorRef){
+   
+  }
+
+  ngOnInit(): void {
+    this.x.set(10) 
+    this.a =7;
+
+    
+    console.log(this.change.detectChanges());
+  }
+  
+  onInc(){
+   
+    this.count.update(e=>e+1)
+    
+  }
+  onDec(){
+    this.count.update(e=>e-1)
+    
+  }
+}
