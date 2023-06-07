fb9d3c3ca0705c5e939e9e24857e050f6d5ffe56
harmik-7span <harmik@7span.com>
2023-06-07 14:03:44 +0530

    console change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
   
   
   constructor(private change:ChangeDetectorRef){
-   console.log('object');
+   console.log('objec');
   }
 
   ngOnInit(): void {
